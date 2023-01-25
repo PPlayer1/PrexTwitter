7a44573f6569e1f61e3b0af4dd8272f7512474de
PPlayer1 <prex.luciano14@gmail.com>
2023-01-25 18:02:45 +0800

    c

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ const test = async(msg)=> {
 
 const rule = new scheduleDM.RecurrenceRule();
 rule.hour =18;
-rule.tz = 'Etc/GMT-8';
+rule.tz = 'Etc/GMT+8';
 
 const job = scheduleDM.scheduleJob(rule,()=>{
     test('Wek');
