dfc3b1bb7625faaa00844ba5fe442d8a97e28369
PPlayer1 <prex.luciano14@gmail.com>
2023-01-25 14:34:37 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+
+require("dotenv").config()
+const express = require("express");
+const rwClient = require("./twitterclient");
+const scheduleDM = require('node-schedule');
+
+const start = ()=> {
+
+    const app = express();
+    const port = process.env.PORT;
+    app.listen (port,()=>{
+        console.log(`server connected to port: ${port}`)
+    })
+}
+start();
+
+
+
+const userId = "860128607108161536-1095227047025115137";
+const test = async(msg)=> {
+    try {
+        await rwClient.v2.sendDmInConversation(userId, {text:msg});
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const rule = new scheduleDM.RecurrenceRule();
+rule.hour =0;
+rule.minute =0;
+rule.second =1;
+rule.tz = 'Etc/GMT-8';
+
+const job = scheduleDM.scheduleJob(rule,()=>{
+    console.log('tite');
+});
